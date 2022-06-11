fc9f86a70a195857c3dfcebefe05ea6603a722d5
Jutipong.Dev <techcom.ec20@gmail.com>
2022-06-11 17:18:19 +0700

    update

--- src/components/Base/DateRange.vue
+++ src/components/Base/DateRange.vue
@@ -3,7 +3,7 @@
     <template v-slot:prepend>
       <q-icon name="event" class="cursor-pointer">
         <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-          <q-date range v-model="date" mask="DD-MM-YYYY">
+          <q-date range :locale="thLocale" v-model="date" mask="DD-MM-YYYY">
             <div class="row items-center justify-end">
               <q-btn v-close-popup label="Close" color="primary" flat />
             </div>
@@ -24,6 +24,16 @@ import { isString } from '@vue/shared';
 const storeMain = useMainLayoutStore();
 let date = $ref({ from: '', to: '' });
 let dateText = $ref('');
+let thLocale = $ref({
+  /* starting with Sunday */
+  days: 'อาทิตย์_วัันทร์_อังคาร_พุธ_พฤหัสบดี_ศุกร์_เสาร์่'.split('_'),
+  daysShort: 'อา_จ_อ_พ_พฤ_ศ_ส'.split('_'),
+  months: 'มกราคม_กุมภาพันธ์_มีนาคม_เมษายน_พฤษภาคม_มิถุนายน_กรกฎาคม_สิงหาคม_กันยายน_ตุลาคม_พฤศจิกายน_ธันวาคม'.split('_'),
+  monthsShort: 'ม.ค._ก.พ._มี.ค._เม.ย._พ.ค._มิ.ย._ก.ค._ส.ค._ก.ย._ต.ค._พ.ย._ธ.ค.'.split('_'),
+  firstDayOfWeek: 0, // 0-6, 0 - Sunday, 1 Monday, ...
+  format24h: true,
+  pluralDay: 'วัน',
+});
 
 //logic
 watchEffect(() => {
