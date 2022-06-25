45cadef2265dedbdc242835648e2971b74d9fbb1
Jutipong.Dev <techcom.ec20@gmail.com>
2022-06-25 23:54:58 +0700

    fix: warning

--- src/pages/CRUD.vue
+++ src/pages/CRUD.vue
@@ -2,12 +2,12 @@
   <q-page class="q-pa-sm">
     <Search />
     <Table />
-    <Modal/>
+    <!-- <Modal/> -->
   </q-page>
 </template>
 
 <script setup>
 import Table from '../components/CRUD/Table.vue';
 import Search from '../components/CRUD/Search.vue';
-import Modal from '../components/CRUD/Modal.vue'
+// import Modal from '../components/CRUD/Modal.vue'
 </script>
